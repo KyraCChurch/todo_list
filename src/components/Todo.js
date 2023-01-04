e6270ed2dec06bd7bc31d10225a86212c761606a
KyraCChurch <kchurch0114@gmail.com>
2023-01-04 16:05:45 -0700

    finishing part 2

---
+++ src/components/Todo.js
@@ -0,0 +1,8 @@
+export default function Todo({ todo, buttonAction, buttonText }){
+    return(
+           <div>{todo.title} 
+                    <button onClick={() => buttonAction(todo._id) }>{buttonText}</button>
+            </div>
+    )
+}
+
